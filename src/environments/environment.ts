a4b8a494d71781b4acdd1bf4b4a4e61ffa74f9ef
isabella_wosniack <isabella.wosniack@gmail.com>
2024-12-15 19:13:00 +0100

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const ENV = {
+    apiUrl: 'http://localhost:3000/api' // URL backend
+}
+
+export const environment = {
+    production: false,
+    ENDPOINT_ARTWORK: `${ENV.apiUrl}/artworks`
+};
